d9bab1b842140c31151effa47903755e031113d2
kwokandy <andy.kwok.work@gmail.com>
2014-07-20 22:53:12 +0200

    Dim 20 Juillet soir calcV2

---
+++ tip_calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+meal=int
+tax
+tip
+tax_value
+meal_with_tax
+tip_value
+total=(meal_with_tax+tip_value)
+
+print "the base cost of the meal is %s, the tax is %s " % (meal
+print "the tax is" + tax_value
+print "the tip will be" + tip
+print "the total of the meal is" + total
+
+
+
