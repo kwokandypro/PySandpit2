0253707a0384f1d59f4d949aeae1ba7558df18da
kwokandy <andy.kwok.work@gmail.com>
2014-07-29 07:34:37 +0200

    Mardi 29 Juillet 2014

--- tip_calc.py
+++ tip_calc.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
-meal=int
-tax
-tip
-tax_value
-meal_with_tax
-tip_value
+meal=int(raw_input("Type your meal's price"))
+tax=0.02
+tip=float(raw_input("What is the tip rate of your meal?"))
+tax_value=tax*meal
+meal_with_tax=meal+tax_value
+tip_value=meal*tip
 total=(meal_with_tax+tip_value)
 
-print "the base cost of the meal is %s, the tax is %s " % (meal
-print "the tax is" + tax_value
-print "the tip will be" + tip
-print "the total of the meal is" + total
+print "the base cost of the meal is %s, the tax is %s " % (meal,tax)
+print "the tax is" + str(tax_value)
+print "the tip will be" + str(tip)
+print "the total of the meal is" + str(total)
 
 
 
